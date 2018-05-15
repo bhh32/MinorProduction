d8545ed871cf4c942a7251351082437d16668e33
Jacob M Miller <s179063@students.aie.edu.au>
2018-05-14 17:39:12 -0700

    Player controller started, basic movement possible

---
+++ Assets/Scripts/Jacob/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    //Player NavMesh
+    private NavMeshAgent agent;
+
+    //Gets NavMesh
+	void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0)){
+
+            //Gets Mouse position relitive to the world from the camera
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+	}
+}
