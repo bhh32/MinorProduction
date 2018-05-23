6a6d728c09eada6dd305a6a3264bf6d16307f42d
Bryan Hyland <bryan.hyland32@gmail.com>
2018-05-23 13:53:37 -0700

    Added UI Action Tool Tips

---
+++ Assets/Game/Scripts/Bryan/UI/UI Actions/HoverToolTip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverToolTip : MonoBehaviour,  IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Text toolTip;
+
+    void Start()
+    {
+        toolTip.enabled = false;
+    }
+
+    #region IPointerEnterHandler implementation
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        toolTip.enabled = true;
+    }
+    #endregion
+    
+    #region IPointerExitHandler implementation
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        toolTip.enabled = false;
+    }
+    #endregion
+}
