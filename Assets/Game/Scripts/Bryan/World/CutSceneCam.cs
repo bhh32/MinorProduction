d414978df3a39ab92e21b67f8c67ee52067eae2a
Bryan Hyland <bryan.hyland32@gmail.com>
2018-06-06 08:13:59 -0700

    Worked On Cut Scene

---
+++ Assets/Game/Scripts/Bryan/World/CutSceneCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneCam : MonoBehaviour 
+{
+    [SerializeField] GameObject cutSceneCam;
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] Animation cutSceneAnim;
+    [SerializeField] AnimationClip fightClip;
+    [SerializeField] Animator cutSceneAnimCont;
+
+    [SerializeField] float timer;
+
+    void Awake()
+    {
+        cutSceneAnim.clip = fightClip;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!cutSceneAnim.isPlaying)
+        {
+            cutSceneAnimCont.SetBool("canPlay", false);
+            mainCamera.SetActive(true);
+            cutSceneCam.SetActive(false);
+        }
+    }
+}
--- Assets/Game/Scripts/Bryan/World/DestroyRodent.cs
+++ Assets/Game/Scripts/Bryan/World/DestroyRodent.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class DestroyRodent : MonoBehaviour 
 {
-    void OnTriggerEnter(Collider other)
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject cutSceneCam;
+    [SerializeField] Animator cutSceneAnimation;
+
+    void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Jungle Rodent") && RodentAI.instance.WasWhipped)
-        {
+        {      
+            cutSceneCam.SetActive(true);
+            mainCamera.SetActive(false);
+
+
+            cutSceneAnimation.SetBool("canPlay", true);
+
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
