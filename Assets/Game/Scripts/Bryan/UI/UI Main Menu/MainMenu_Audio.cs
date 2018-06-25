2f4e0f1df3a6881bdf26dc52a045f52f20e5e8d8
Bryan Hyland <bryan.hyland32@gmail.com>
2018-06-25 08:59:04 -0700

    Added Main Menu Content

---
+++ Assets/Game/Scripts/Bryan/UI/UI Main Menu/MainMenu_Audio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Audio : MonoBehaviour 
+{
+    [SerializeField] AudioSource audio;
+    [SerializeField] AudioClip clip;
+
+    void Start()
+    {
+        audio.Play();
+        if (!audio.loop)
+            audio.loop = true;
+    }
+}
