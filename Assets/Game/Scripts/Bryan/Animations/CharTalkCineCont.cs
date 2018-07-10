d4450ad2a31d4b25a024ba93c6603638f5f574d8
Bryan Hyland <bryan.hyland32@gmail.com>
2018-07-10 16:07:31 -0700

    Added Talking To Cine Still Needs Work

---
+++ Assets/Game/Scripts/Bryan/Animations/CharTalkCineCont.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharTalkCineCont : MonoBehaviour 
+{
+    [SerializeField] Animator anim;
+
+    void OnEnable()
+    {
+        if (!anim.enabled)
+            anim.enabled = true;
+
+        if (anim.GetBool("isTalking"))
+            anim.SetBool("isTalking", false);
+        else
+            anim.SetBool("isTalking", true);
+    }
+}
